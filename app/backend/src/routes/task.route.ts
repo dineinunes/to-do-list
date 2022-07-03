ed1be8d63f432b48704bef3269b0f46d25f24826
dineinunes <nunesvieiraclaudinei@outlook.com>
2022-07-03 20:18:31 -0300

    FIX: Further configuration of route files

--- app/backend/src/routes/task.route.ts
+++ app/backend/src/routes/task.route.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import TaskController from '../controllers/task.controller';
 
 const task = Router();
 
-task.get('/'); // get all tasks
+const controller = new TaskController();
+
+task.get('/', controller); // get all tasks
 task.post('/'); // create new task
 task.put('/'); // update general informations of a task
 task.put('/'); // update status of a task
--- app/backend/src/routes/user.route.ts
+++ app/backend/src/routes/user.route.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import UserController from '../controllers/user.controller';
 
 const user = Router();
+
+const controller = new UserController();
 
 user.post('/'); // create new user
 user.post('/'); // log in a user

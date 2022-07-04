34c8d4d02653d8f4f02584a85f9bbded233db7df
dineinunes <nunesvieiraclaudinei@outlook.com>
2022-07-03 23:17:43 -0300

    FEAT: Implements methods for task service

--- app/backend/src/services/task.service.ts
+++ app/backend/src/services/task.service.ts
@@ -1,3 +1,5 @@
+import { UpdateQuery } from 'mongoose';
+import ITask from '../models/interfaces/task.interface';
 import TaskModel from '../models/task.model';
 
 export default class TaskService {
@@ -5,5 +7,41 @@ export default class TaskService {
 
   constructor() {
     this.model = new TaskModel();
+  }
+
+  public async createTask(data: ITask):
+  Promise<number> {
+    await this.model.createTask(data);
+    return 201;
+  }
+
+  public async getAllTasks(username: string):
+  Promise<number> {
+    await this.model.getAllTasks(username);
+    return 200;
+  }
+
+  public async updateTask(id: string, data: UpdateQuery<ITask>):
+  Promise<number> {
+    await this.model.updateTask(id, data);
+    return 200;
+  }
+
+  public async updateStatus(id: string, status: UpdateQuery<ITask>):
+  Promise<number> {
+    await this.model.updateStatus(id, status);
+    return 200;
+  }
+
+  public async deleteTask(id: string):
+  Promise<number> {
+    await this.model.deleteTask(id);
+    return 204;
+  }
+
+  public async deleteAllTasks(username: string):
+  Promise<number> {
+    await this.model.deleteTask(username);
+    return 204;
   }
 }
